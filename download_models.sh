70d6d4ff25315dec38799df3e08fe392b5bcdf22
Tianfan Xue <tfxue@thursday.csail.mit.edu>
2017-05-29 12:22:54 -0400

    download scripts

---
+++ download_models.sh
@@ -0,0 +1,4 @@
+mkdir -p models
+
+wget http://visualdynamics.csail.mit.edu/repo/exercise128_model.t7 -O models/exercise128_model.t7
+wget http://visualdynamics.csail.mit.edu/repo/exercise128_z.t7 -O models/exercise128_z.t7
